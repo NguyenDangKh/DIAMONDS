97dd09f21be67d20982562c5f9abc2f6b8b6e720
khoang <139105054+NguyenDangKh@users.noreply.github.com>
2024-05-22 22:45:13 +0700

    Delete DIAMOND directory

--- DIAMOND/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DIAMOND.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
